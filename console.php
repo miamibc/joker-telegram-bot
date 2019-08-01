09abc46f8ab27f318b3c937533142c06a49f2c04
Sergei Miami <miami@blackcrystal.net>
2019-08-01 10:34:30 +0200

    5 messages between stickers

--- console.php
+++ console.php
@@ -22,66 +22,88 @@ $bot = new \TelegramBot\Api\Client( $token );
 
 /** @var $bot \TelegramBot\Api\BotApi */
 
-$update = false;
-foreach ( $bot->getUpdates( $last_update ) as $update)
+do
 {
 
-  echo "\n" . $update->toJson();
-
-  $message = $update->getMessage();
-  if (!$message) $message = $update->getEditedMessage();
-  if (!$message) $message = $update->getChannelPost();
-  if (!$message) $message = $update->getEditedChannelPost();
-
-  $channel = $message->getChat()->getTitle();
-
-  if (!in_array( $channel, $channels )) { continue; }
-
-  $author_id  = $message->getFrom()->getId();
-  $channel_id = $message->getChat()->getId();
-  $counter    = $redis->incr( "joker_channel_{$channel_id}_counter" );
-
-  echo " Counter $counter";
-
-  if ($message->getSticker() && $counter < 5)
+  $update = false;
+  foreach ($bot->getUpdates($last_update) as $update)
   {
 
-    $author = trim($message->getFrom()->getFirstName().' '.
-                   $message->getFrom()->getLastName());
-    $jokes = [
-      'Это бан :p',
-      'Печалька :DDD',
-      'Вытрись, ты умер!',
-      'Хорошая попытка, чувак :p',
-      'Ничего личного...',
-      'Выкуси :-E',
-      'Ну и что? Вот и всё!',
-      '1:0 ты проиграл :p',
-      'Шах и мат',
-    ];
-    $joke = $jokes[array_rand($jokes)];
-    // $answer  = "Sticker flood detected $author лишился стикера $counter-й раз. $joke";
-    // $bot->sendMessage( $channel, $answer );
+    $message = $update->getMessage();
+    if (!$message)
+      $message = $update->getEditedMessage();
+    if (!$message)
+      $message = $update->getChannelPost();
+    if (!$message)
+      $message = $update->getEditedChannelPost();
 
-    try
+    $channel = $message->getChat()->getTitle();
+    if (!in_array($channel,$channels))
     {
-      $bot->deleteMessage(
-        $message->getChat()->getId(),
-        $message->getMessageId()
-      );
-      echo " Deleted";
-    }
-    catch (\TelegramBot\Api\HttpException $exception)
-    {
-      echo " $exception";
+      continue;
     }
 
-    $redis->set( "joker_channel_{$channel_id}_counter", 0 );
+    echo "\n".$update->toJson();
+
+    $author_id = $message->getFrom()->getId();
+    $channel_id = $message->getChat()->getId();
+
+    $counter = $redis->incr($key = "joker_channel_{$channel_id}_counter");
+
+    if ($message->getSticker())
+    {
+      if ($counter < 0)
+      {
+        echo " sticker deleted";
+        /*
+        $author = trim($message->getFrom()->getFirstName().' '.$message->getFrom()->getLastName());
+        $jokes = [
+          'Это бан :p',
+          'Печалька :DDD',
+          'Вытрись, ты умер!',
+          'Хорошая попытка, чувак :p',
+          'Ничего личного...',
+          'Выкуси :-E',
+          'Ну и что? Вот и всё!',
+          '1:0 ты проиграл :p',
+          'Шах и мат',
+        ];
+        $joke = $jokes[array_rand($jokes)];
+        $answer  = "Sticker flood detected $author лишился стикера $counter-й раз. $joke";
+        $bot->sendMessage( $channel, $answer );
+        */
+
+        try
+        {
+          $bot->deleteMessage(
+            $message->getChat()->getId(),
+            $message->getMessageId()
+          );
+          echo " Deleted";
+        }
+        catch (\TelegramBot\Api\HttpException $exception)
+        {
+          echo " $exception";
+        }
+
+      }
+
+      $redis->set($key, $counter = -5);
+
+    }
+
+    // some stats here
+    echo " counter $counter";
 
   }
-}
 
-if ($update)
-{
-  $redis->set('joker_last_update', $update->getUpdateId()+1);
-}
+  if ($update)
+  {
+    $last_update = $update->getUpdateId() + 1;
+    $redis->set('joker_last_update', $last_update);
+  }
+
+  sleep(3);
+
+
+} while (true);
