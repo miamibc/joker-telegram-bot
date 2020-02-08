7f2f28eace52dbf0fe4ed04e930f93cb47877552
Sergei Miami <miami@blackcrystal.net>
2020-02-09 00:00:12 +0100

    Little helpers

---
+++ joker-getfile.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package joker-telegram-bot
+ * @author Sergei Miami <miami@blackcrystal.net>
+ */
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(dirname(__FILE__));
+$dotenv->load();
+
+$token = getenv( 'TELEGRAM_TOKEN' );
+$file_id = $argv[1];
+
+$bot = new Joker\Bot( $token );
+
+$result = $bot->customRequest('getFile', ['file_id'=>$file_id]);
+
+if (!isset($result['file_path']))
+  throw new Exception("file_path not found");
+
+$data = file_get_contents( "https://api.telegram.org/file/bot$token/$result[file_path]" );
+if (!$data)
+  throw new Exception("Empty file");
+
+echo($data);
---
+++ joker-viewlog.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package joker-telegram-bot
+ * @author Sergei Miami <miami@blackcrystal.net>
+ */
+
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::create(dirname(__FILE__));
+$dotenv->load();
+
+$stdin = fopen('php://stdin', 'r');
+$stdout = fopen('php://stdout', 'w');
+while ( ($data = fgets( $stdin )) !== false)
+{
+  $line = json_decode( $data, true );
+  $text = isset( $line['message']['text']) ? $line['message']['text'] : $data;
+  $author = isset( $line['message']['from']['username']) ? @$line['message']['from']['username'] : @$line['message']['from']['first_name'];
+  $date   = isset( $line['message']['date']) ? date( 'Y-m-d H:i:s', $line['message']['date'] ): 'no date';
+  fputs( $stdout, "[$date] <$author> $text\n");
+}
+fclose( $stdin );
+fclose( $stdout );
