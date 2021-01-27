28eedbd27c08b03a9bc5c680f237c7b2140d303f
Sergei Miami <miami@blackcrystal.net>
2021-01-27 14:09:15 +0200

    Dob added

---
+++ src/Animal/Dob.php
@@ -0,0 +1,19 @@
+<?php
+namespace Joker\Animal;
+
+use Cowsayphp\AbstractAnimal;
+
+class Dob extends AbstractAnimal
+{
+    protected $character = <<<DOC
+
+{{bubble}}
+            \
+            __
+   ~ ______/ V`-,
+    }        /~~
+   /_)^ --,r'
+  |_,     |_,
+   
+DOC;
+}
