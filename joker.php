84c2de1f667bdcb4a873559724a606bf30d08588
Sergei Miami <miami@blackcrystal.net>
2021-08-31 02:46:27 +0300

    Added options

--- joker.php
+++ joker.php
@@ -13,7 +13,7 @@ $bot->plug([
 
   new Joker\Plugin\Log( ['file' =>'data/log/log.json'] ),
   new Joker\Plugin\Activity( ['sync_time' => 60] ),
-  new Joker\Plugin\Kicker(),
+  new Joker\Plugin\Kicker(['secons_with_emoji' => 0, 'secons_without_emoji' => 600]),
   new Joker\Plugin\Forwarder( [
     ['from' => -343502518, 'text' => ['*покуп*'], 'to' => -343502518, ],
     ['from' => -343502518, 'text' => ['*прода*', '*сдаё*'], 'to' => -343502518, 'forward' => false ],
