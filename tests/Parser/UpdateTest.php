4000a3973251aee5c14daa49320f72fcc3e6ee63
Sergei Miami <miami@blackcrystal.net>
2021-11-02 23:36:53 +0200

    Access bot from any parser

--- tests/Parser/UpdateTest.php
+++ tests/Parser/UpdateTest.php
@@ -7,6 +7,7 @@
 
 namespace Tests\Parser;
 
+use Joker\Bot;
 use Joker\Parser\Chat;
 use Joker\Parser\ChatPhoto;
 use Joker\Parser\Message;
@@ -18,14 +19,24 @@ class UpdateTest extends TestCase
 
   public function testParent()
   {
-    $update = new Update(['message' => ['chat' => ['photo' => ['small_file_id' => 'test']]]]);
+    $bot = new Bot(false);
+    $update = new Update(['message' => ['chat' => ['photo' => ['small_file_id' => 'test']]]], $bot);
+
     $this->assertInstanceOf( Message::class, $element = $update->message() );
+    $this->assertInstanceOf( Bot::class, $element->bot() );
+
     $this->assertInstanceOf( Chat::class, $element = $element->chat() );
+    $this->assertInstanceOf( Bot::class, $element->bot() );
+
     $this->assertInstanceOf( ChatPhoto::class, $element = $element->photo() );
+    $this->assertInstanceOf( Bot::class, $element->bot() );
+
+    // back to bot manually
     $this->assertInstanceOf( Chat::class, $element = $element->parent() );
     $this->assertInstanceOf( Message::class, $element = $element->parent() );
     $this->assertInstanceOf( Update::class, $element = $element->parent() );
-    $this->assertNull( $element->parent() );
+    $this->assertInstanceOf( Bot::class, $element->parent() );
   }
+
 
 }
