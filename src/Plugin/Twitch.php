d3fa4a684c2efd37a059e0bc01f4835d9642020a
Sergei Miami <miami@blackcrystal.net>
2023-11-18 02:09:59 +0200

    Helper Strings::diffTimeInWords used

--- src/Plugin/Twitch.php
+++ src/Plugin/Twitch.php
@@ -12,6 +12,7 @@
 
 namespace Joker\Plugin;
 
+use Joker\Helper\Strings;
 use Joker\Parser\Update;
 
 class Twitch extends Base
@@ -67,7 +68,7 @@ class Twitch extends Base
         // title and a link
         "<a href=\"https://www.twitch.tv/{$item['display_name']}\">" . trim($item['title']) . "</a>",
         // started time
-        $item['started_at'] ? ' started '.self::time_elapsed($item['started_at']) : ''
+        $item['started_at'] ? ' started '. Strings::diffTimeInWords($item['started_at'], time()) . ' ago' : ''
       ]));
     }
 
@@ -122,43 +123,6 @@ class Twitch extends Base
     ]]));
 
     return json_decode( $json, true );
-  }
-
-  /**
-   * @param $datetime
-   * @param false $full
-   *
-   * @return string
-   * @throws \Exception
-   */
-  public static function time_elapsed($datetime, $full = false)
-  {
-    $now = new \DateTime;
-    $ago = new \DateTime($datetime);
-    $diff = $now->diff($ago);
-
-    $diff->w = floor($diff->d / 7);
-    $diff->d -= $diff->w * 7;
-
-    $string = array(
-      'y' => 'year',
-      'm' => 'month',
-      'w' => 'week',
-      'd' => 'day',
-      'h' => 'hour',
-      'i' => 'minute',
-      's' => 'second',
-    );
-    foreach ($string as $k => &$v) {
-      if ($diff->$k) {
-        $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
-      } else {
-        unset($string[$k]);
-      }
-    }
-
-    if (!$full) $string = array_slice($string, 0, 1);
-    return $string ? implode(', ', $string) . ' ago' : 'just now';
   }
 
 }
